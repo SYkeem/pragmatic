98f4ee2cda3cd135a4d98421585cab63dfa9a314
sykeem <ahr03003@gmail.com>
2021-09-05 20:23:55 +0900

    django course 28 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,15 +1,20 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 # Create your views here.
 from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from accountapp.decorates import account_ownership_required
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
 
+has_ownership = [login_required, account_ownership_required]
 
+@login_required
 def hello_world(request):
     if request.method == "POST":
         temp = request.POST.get('hello_world_input')
@@ -24,6 +29,7 @@ def hello_world(request):
 
         return render(request, 'accountapp/hello_world.html',context={'hello_world_list': hello_world_list})
 
+
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
@@ -35,6 +41,8 @@ class AccountDetailView(DetailView):
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
 
+@method_decorator(has_ownership,'get')
+@method_decorator(has_ownership,'post')
 class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountUpdateForm
@@ -42,6 +50,9 @@ class AccountUpdateView(UpdateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
 
+
+@method_decorator(has_ownership,'get')
+@method_decorator(has_ownership,'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
