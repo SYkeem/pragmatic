1a2688ca2d2d5b8e815f393fb9c00910d5d18672
sykeem <ahr03003@gmail.com>
2021-09-15 13:08:15 +0900

    django course 40 commit

---
+++ projectapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from commentapp.views import CommentCreateView, CommentDeleteView
+from projectapp.views import ProjectDetailView, ProjectCreateView, ProjectListView
+
+app_name = 'projectapp'
+
+urlpatterns = [
+    path('list/', ProjectListView.as_view(), name='list'),
+    path('create/', ProjectCreateView.as_view(), name='create'),
+    path('detail/<int:pk>', ProjectDetailView.as_view(), name='detail'),
+    ]
